0e781774a8e1b3a9fe87f98e45b39a4459ff3a63
Glitch (hello-express) <none>
2021-04-20 09:24:18 +0000

    👛💋 Checkpoint ./routes/diseases.js:13970934/352 ./server.js:13970934/519

--- routes/diseases.js
+++ routes/diseases.js
@@ -5,23 +5,22 @@ const axios = require('axios');
 const OutbreakAPI = require('../services/outbreak-api');
 
 async function myfunc(start_date, end_date, keyterms, undefined) {
-    console.log(start_date);
     const outbreakAPI = new OutbreakAPI(
         start_date,
         end_date,
         keyterms,
         undefined
     );
-    // console.log(outbreakAPI);
     const articles = await outbreakAPI.getArticles();
+    console.log(articles);
     return articles;
 }
 
 const router = express.Router();
-router.get('/', (request, response) => {
+router.get('/diseases', (request, response) => {
     const start_date = request.query.start_date;
     const end_date = request.query.end_date;
-    const keyterms = request.query;
+    const keyterms = request.query.keyterms;
     const outbreakAPI = new OutbreakAPI(
         start_date,
         end_date,
--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const webviewRoutes = require('./routes/webviews');
-const verificationRoutes = require('./routes/verification');
 const diseaseRoutes = require('./routes/diseases');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,31 +10,22 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 
 // GET endpoint
-app.get('/', function(request, response) {
-    response.send('Hi, welcome to our home page.');
-});
-
-app.use('/diseases', diseaseRoutes);
-app.get('/diseases', function(request, response, next) {
+app.use('/', diseaseRoutes);
+app.get('/', function(request, response, next) {
     next();
 });
 
-app.post('/attributes/reset', (request, response) => {
-    const keyNames = Object.keys(request.body);
 
-    let attributes = {};
-    keyNames.forEach(keyName => {
-        attributes[keyName] = null;
-    });
+// app.post('/attributes/reset', (request, response) => {
+//     const keyNames = Object.keys(request.body);
 
-    response.json({ set_attributes: attributes });
-});
+//     let attributes = {};
+//     keyNames.forEach(keyName => {
+//         attributes[keyName] = null;
+//     });
 
-app.use('/webviews', webviewRoutes);
-
-// Add verification routes
-app.use('/verify', verificationRoutes);
-
+//     response.json({ set_attributes: attributes });
+// });
 
 // Start the server and listen for incoming requests
 const listener = app.listen(process.env.PORT, function() {
