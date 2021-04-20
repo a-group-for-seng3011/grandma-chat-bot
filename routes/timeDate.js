b84ca8e615bb11a7c1c7c0534eac4f02457ac6e8
Glitch (hello-express) <none>
2021-04-20 05:25:21 +0000

    🛤📖 Checkpoint ./routes/timeDate.js:31126911/9675

--- routes/timeDate.js
+++ routes/timeDate.js
@@ -1,23 +1,20 @@
 const express = require('express');
-const axios = require('axios');
-const OutbreakAPI = require("./services/outbreak-api");
 
 const router = express.Router();
 
 router.get('/', (request, response) => {
+    const start_date = request.query;
+    const end_date = request.query;
+    const keyterms = request.query;
   
-  
-  const start_date = request.query;
-  const end_date = request.query;
-  const keyterms = request.query;
-  
-  const outbreakAPI = new OutbreakAPI(start_date, end_date, keyterms, undefined);
-  const articles = outbreakAPI.getSortedArticles();
-  const userAttributes = {
-    set_attributes: articles
+  const dateObject = {
+    articles: "daaaw"
   };
-  console.log("hey");
-
+    
+  const userAttributes = {
+    set_attributes: dateObject
+  };
+  
   response.json(userAttributes);
 });
 
