b4b1e84de8e84d5d71ff76fddd3dc8564ffa413a
Glitch (hello-express) <none>
2021-04-20 05:13:15 +0000

    👽🏦 Checkpoint ./routes/timeDate.js:31107543/910 ./routes/outbreak-api.js:31107543/1262 ./routes/config.js:31107543/3234

---
+++ routes/outbreak-api.js
@@ -0,0 +1,44 @@
+"use strict";
+
+// Imports dependencies
+const axios = require("axios"),
+    config = require("./config");
+
+module.exports = class OutbreakAPI {
+
+    // Set positional parameters to undefined for the default values
+    constructor(start_date="0001-01-01T00:00:00",
+        end_date="2099-12-31T23:59:59",
+        keyterms,
+        location) {
+            this.start_date = start_date;
+            this.end_date = end_date;
+            this.keyterms = keyterms;
+            this.location = location;
+        }
+
+    async getArticles() {
+        const params = {
+            start_date: this.start_date,
+            end_date: this.end_date,
+            keyterms: this.keyterms,
+            location: this.location,
+            token: config.outbreakToken
+        }
+        // Send the HTTP request to the Outbreak API
+        try {
+            const response = await axios.get(config.outbreakUrl, { params });
+            console.log(response);
+
+            return response.data.Reports;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getSortedArticles() {
+        const articles = await this.getArticles();
+
+        return articles.sort((a, b) => Date.parse(b.date_of_publication) - Date.parse(a.date_of_publication));
+    }
+}
--- routes/timeDate.js
+++ routes/timeDate.js
@@ -1,36 +1,23 @@
 const express = require('express');
-const express = require('axios');
+const axios = require('axios');
+const OutbreakAPI = require("./services/outbreak-api");
 
 const router = express.Router();
 
 router.get('/', (request, response) => {
+  
+  
   const start_date = request.query;
   const end_date = request.query;
   const keyterms = request.query;
-  const token = request.query;
-  let url = `https://disease-reports-api.herokuapp.com/diseases/?start_date=${start_date}&end_date=${end_date}&keyterms=${keyterms}&token=${token}`;
-  const userDateInTimezone = axios.get(url)
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
   
-  const dateObject = {
-    day: userDateInTimezone.getDate(),
-    month: userDateInTimezone.getMonth() + 1,
-    year: userDateInTimezone.getFullYear(),
-    hours: userDateInTimezone.getHours(),
-    minutes: userDateInTimezone.getMinutes(),
-    seconds: userDateInTimezone.getSeconds(),
-    isoTime: userDateInTimezone.toISOString()
-  };
-    
+  const outbreakAPI = new OutbreakAPI(start_date, end_date, keyterms, undefined);
+  const articles = outbreakAPI.getSortedArticles();
   const userAttributes = {
-    set_attributes: userDateInTimezone
+    set_attributes: articles
   };
-  
+  console.log("hey");
+
   response.json(userAttributes);
 });
 
